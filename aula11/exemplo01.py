83dc93ae8df23ad2ee5b6145ff89fcc3fff0eca8
AsafeSias <asafesias@gmail.com>
2026-01-06 19:32:58 -0300

    Gerando lista de números

--- aula11/exemplo01.py
+++ aula11/exemplo01.py
@@ -13,3 +13,8 @@ n_decimal = random.uniform(1,10)
 numero_decimal = round(n_decimal,3) #Arredonda
 print(f'{n_decimal:.2f}') #Formatação para duas casas
 print(numero_decimal)
+
+#---------------------------------------
+# Gerar número aleatórios dentro de uma lista
+lst_numeros = [random.randint(1,10) for num in range(5)]
+print(lst_numeros)
